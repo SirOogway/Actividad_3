10bb76a964bf34e66d25d359fcdc6f74e7130def
SirOogway <jesusandresrodriguezgarcia@outlook.com>
2023-07-26 16:06:12 -0500

    css added

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php
-include("./biblioteca.php");
+include("biblioteca.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
     <title>Trabajo 3</title>
 </head>
 
 <body>
-    <h1>Trabajo 3</h1>
-    <?php
-    $num1 = 5;
-    $num2 = 2;
+    <div class="container">
+        <div class="tarjeta tarjeta-beauty">
+            <h1>Trabajo 3</h1>
+            <?php
+            $num1 = 5;
+            $num2 = 2;
+            ?>
+            <?php
+            echo '<h2>El resultado es: ', operaciones('M', num1: $num1, num2: $num2), '</h2>';
+            ?>
+        </div>
+    </div>
 
-    echo '<h2>El resultado es: ', operaciones('x', num1:$num1, num2:$num2), '</h2>';
-    ?>
 </body>
 
 </html>
